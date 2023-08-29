eb708ad8dd95e501741feea245b2b8ecfaf93e21
flores0A <armellaf20@gmail.com>
2023-08-29 09:28:17 -0400

    cambios

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,8 +1,7 @@
-
-import NextAuth from "next-auth";
+import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github";
 import { MongoDBAdapter } from "@auth/mongodb-adapter"
-import {clientPromise} from "lib/mongodb"
+import clientPromise from "../../../lib/mongodb"
 
 
 export default NextAuth({
